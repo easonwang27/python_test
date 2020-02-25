7e36365a4303c0aa33278f50f99f48f5765fcfd7
eason wang <easonwang27@163.com>
2020-02-25 11:42:51 +0800

    input argv

---
+++ input_sys.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+#-*-coding :UTF-8-*-
+import sys
+print sys.argv
---
+++ printx.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#-*-coding :UTF-8-*-
+x="a"
+y="b"
+print x
+print y
+print '----------'
+print x,
+print y,
+print x,y
+

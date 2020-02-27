a1c221ec6e0b9051694ac3bacd942c6b8ee66ea1
eason wang <easonwang27@163.com>
2020-02-27 17:05:49 +0800

    python arry string

---
+++ python_arr2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*-coding:UTF-8 -*-
+list = ['runoob',785,2.23,'john',70.2]
+tinylist=[123,'john']
+pp="eason"
+chr="yes"
+print list
+print list[0]
+print list[1:3]
+print list[2:]
+print tinylist*2
+print list+tinylist
+print pp
+print chr
